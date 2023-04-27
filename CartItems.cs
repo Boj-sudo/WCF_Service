311b1ec2e8520230a2b920983a2de596e966d5aa
Boj-sudo <tsietsibojane7@gmail.com>
2023-04-27 19:32:23 +0200

    Added changes to the database

---
+++ CartItems.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WCF_Service
+{
+    public class CartItems
+    {
+        public int p_code;
+        public int Quantity;
+    }
+}
